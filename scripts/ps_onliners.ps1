dbd197be6b1bb70603df8764b3d690fe94ade74a
Rupert <rupert.broad@gmail.com>
2019-06-19 11:20:25 +0200

    .

--- scripts/ps_onliners.ps1
+++ scripts/ps_onliners.ps1
@@ -1,2 +1,5 @@
 # Tail log file
 Get-Content c:\chef\log\client.log -Wait -Tail 30
+
+# get ad groups for user
+Get-ADPrincipalGroupMembership username | select name
--- scripts/setup_sshkey.sh
+++ scripts/setup_sshkey.sh
@@ -1,5 +1,7 @@
-ssh-keygen -t rsa -b 4096 -C "rupert.broad@email;.com"
+NAME=$1
+
+ssh-keygen -t rsa -b 4096 -C "rupert.broad@ohpen.com" -f ~/.ssh/$NAME
 
 eval "$(ssh-agent -s)"
 
-ssh-add ~/.ssh/id_rsa
+ssh-add ~/.ssh/$NAME
